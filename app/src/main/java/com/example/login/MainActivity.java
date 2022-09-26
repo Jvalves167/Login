574431342928e9283f6b56f2854d3c717ca13a51
Jvalves167 <jvalves167@gmail.com>
2022-09-26 17:02:20 -0300

    0.4

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.login;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvcadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        getSupportActionBar().hide();
+
+        tvcadastrar = findViewById(R.id.tvcadastrar);
+
+        tvcadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent it = new Intent(MainActivity.this, CadastrarActivity.class);
+                startActivity(it);
+
+
+            }
+        });
+
+
+
+
     }
 }
